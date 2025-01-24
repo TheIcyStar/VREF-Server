d1cc17523fc9707ec98971b36753fccdb1ef3eb4
evb5593 <182671131+evb5593@users.noreply.github.com>
2025-01-23 19:39:57 -0800

    feat: added an alternate test mapping

--- src/main/java/edu/psu/vref_server/Controllers/ExampleController.java
+++ src/main/java/edu/psu/vref_server/Controllers/ExampleController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ExampleController {
 
+    @GetMapping("/apitest-alt")
+    String getTest(){
+        return "Goodbye World";
+    }
+
     @GetMapping("/apitest")
     String getTest(){
         return "Hello World";
